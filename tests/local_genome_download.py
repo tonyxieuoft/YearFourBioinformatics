16467935c416d32fc701729d1d0b93d7bc705a30
tonyxieuoft <xiaohan.xie@mail.utoronto.ca>
2024-06-11 22:11:41 -0400

    added blastn command

--- tests/local_genome_download.py
+++ tests/local_genome_download.py
@@ -21,9 +21,16 @@ if __name__ == "__main__":
     os.system(r"datasets download genome accession " + accession)
     os.system("unzip ncbi_dataset.zip")
     os.system("rm ncbi_dataset.zip")
+    os.system("rm README.md")
     genome_file_folder = "ncbi_dataset/data/" + accession
     genome_file = subprocess.check_output(["ls", genome_file_folder]).\
         decode("utf-8").strip()
     print("This is the genome file: " + genome_file)
+    os.system("mkdir orca")
+    os.system("makeblastdb -dbtype nucl -in " + genome_file_folder +
+              "/" + genome_file + " -out orca/orca")
+    os.system("export BLASTDB=${BLASTDB}:$(pwd)/orca")
+    query = "/mnt/c/Users/tonyx/Downloads/'query_files (2)'/9721.fas"
+    os.system("blastn -db orca -outfmt 5 -query " + query + " > test.xml")
 
 
